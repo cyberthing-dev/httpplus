187d6ff501bcec66f0edeaf695c0ab1bfb4ad3d9
purplelemons-dev <ironbreaker2005@gmail.com>
2023-02-11 00:25:15 -0600

    todos and test update

--- src/http_plus_purplelemons_dev/__init__.py
+++ src/http_plus_purplelemons_dev/__init__.py
@@ -22,8 +22,13 @@ In order to access /, the server will look for ./pages/.html. Smiliar thing for 
 You can customize error pages 
 """
 
+__dev_version__ = "0.0.2"
+
+
 # TODO: Add a `-m` script that automatically creates a base directory.
 # ^ either that or serve pre-defined responses from hardcoded text
 
 # TODO: Add configure error pages to include f"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{code}"
 # for errors in the 4XX-5XX range.
+
+# TODO: Initialize server via decorators (@server.get, @server.post, etc.)
--- tests/server.py
+++ tests/server.py
@@ -1,4 +1,6 @@
 
 import http_plus_purplelemons_dev
 
-print(http_plus_purplelemons_dev)
+print(f"Running dev pre-build v{http_plus_purplelemons_dev.__dev_version__}")
+
+Server = http_plus_purplelemons_dev()
