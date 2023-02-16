f20b3a29fb2706bbf8e5a5e4800453058bfeffcf
purplelemons-dev <ironbreaker2005@gmail.com>
2023-02-15 20:06:10 -0600

    poopy update stuff

--- src/http_plus_purplelemons_dev/server.py
+++ src/http_plus_purplelemons_dev/server.py
@@ -13,6 +13,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="A simple HTTP server.")
     parser.add_argument("-p", "--port", type=int, default=8080, help="The port to listen on.")
     parser.add_argument("-d", "--debug", action="store_true", help="Enables debug mode.")
+    parser.add_argument("--bind", type=str, default="0.0.0.0", help="The host IP to listen on.")
     args = parser.parse_args()
 
     server = http_plus.Server("0.0.0.0", args.port, debug=args.debug)
