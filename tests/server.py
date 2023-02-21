a163552252dba86c63ef4d6b7bca50e2da5184c2
purplelemons-dev <ironbreaker2005@gmail.com>
2023-02-21 10:18:38 -0600

    v0.0.5: wildcards and keyword paths

--- tests/server.py
+++ tests/server.py
@@ -2,8 +2,18 @@
 import http_plus_purplelemons_dev as http_plus
 
 server = http_plus.Server("0.0.0.0", 80, debug=True)
+
 @http_plus.get(server, "/")
 def _(req:http_plus.Request, res:http_plus.Response):
     return res.set_body("Hello, world!")
+
+@http_plus.get(server, "/test")
+def _(req:http_plus.Request, res:http_plus.Response):
+    return res.set_body("Hello, test!")
+
+@http_plus.get(server, "/kwtest/:testnum")
+def _(req:http_plus.Request, res:http_plus.Response, *, testnum):
+    print(testnum)
+    return res.set_body(f"Hello, {testnum=}!")
 
 server.listen()
